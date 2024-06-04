f396190299349617ac7e23099161e3055ce5df6f
bautista00 <bautistaquesada00@gmail.com>
2024-06-04 17:11:07 -0300

    AWS config class done & aws dependency agg

--- ms-events/src/main/java/com/proyectointegrador/msevents/config/AwsConfig.java
+++ ms-events/src/main/java/com/proyectointegrador/msevents/config/AwsConfig.java
@@ -1,10 +1,17 @@
 package com.proyectointegrador.msevents.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsConfig {
+
     @Value("${server.aws.access_key_id}")
     private String awsAccessKey;
 
@@ -14,5 +21,10 @@ public class AwsConfig {
     @Value("${server.aws.s3.region}")
     private String awsRegion;
 
-    
+    @Bean
+    public AmazonS3 getS3Client(){
+        BasicAWSCredentials credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+        return AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(awsRegion))
+                .withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+    }
 }
