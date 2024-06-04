2b3c871a956a1b411a8b246b04df4bb934be94c4
bautista00 <bautistaquesada00@gmail.com>
2024-06-04 17:31:12 -0300

    Improvment of aws security

--- ms-events/src/main/java/com/proyectointegrador/msevents/config/AwsConfig.java
+++ ms-events/src/main/java/com/proyectointegrador/msevents/config/AwsConfig.java
@@ -1,30 +1,19 @@
 package com.proyectointegrador.msevents.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsConfig {
 
-    @Value("${server.aws.access_key_id}")
-    private String awsAccessKey;
-
-    @Value("${server.aws.secret_access_key}")
-    private String awsSecretKey;
-
-    @Value("${server.aws.s3.region}")
-    private String awsRegion;
-
     @Bean
-    public AmazonS3 getS3Client(){
-        BasicAWSCredentials credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
-        return AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(awsRegion))
-                .withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of("sa-east-1"))
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
     }
 }
